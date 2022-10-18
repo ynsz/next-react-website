c8bb3954e8c62dee9a55ea806ae2b2628b42c445
ynsz <suzuki.yuna.0801@gmail.com>
2022-10-18 17:51:39 +0900

    first commit

---
+++ components/layout.js
@@ -0,0 +1,17 @@
+import Container from 'components/container'
+import Header from 'components/header'
+import Footer from 'components/footer'
+
+export default function Layout({ children }) {
+  return (
+    <>
+      <Header />
+
+      <main>
+        <Container>{children}</Container>
+      </main>
+
+      <Footer />
+    </>
+  )
+}
